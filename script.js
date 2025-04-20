fd885305b8f4b88c7d06cf319f03defd2455e052
Ryan Umoso <ryanumoso04@gmail.com>
2025-04-20 09:21:48 -0400

    Added comments to Inventory

--- script.js
+++ script.js
@@ -56,17 +56,22 @@ class Store {
     }
   }
 
+  // Inventory System
+
+
   const inventoryOutput = document.getElementById("inventoryOutput");
   const searchOutput = document.getElementById("searchOutput");
   
+  // Create regular products
   const apple = new ProductProperties("Apple", 2.5, 50);
   const chair = new ProductProperties("Chair", 35.0, 10);
   const tv = new ProductProperties("TV", 400.0, 5);
 
-  
+  // Create perishable products
   const milk = new PerishableProductProperties("Milk", 1.5, 10, "2025-05-01");
   const yogurt = new PerishableProductProperties("Yogurt", 0.99, 20, "2025-04-25");
 
+  // Create a new store and the products to its inventory
   const store = new Store();
   store.addProduct(apple);
   store.addProduct(chair);
@@ -74,18 +79,24 @@ class Store {
   store.addProduct(milk);
   store.addProduct(yogurt);
 
+  // Show inventory before discount
   inventoryOutput.innerText += "Inventory System\n\n";
   inventoryOutput.innerText += `Before Discount: $${store.getInventoryValue().toFixed(2)}\n\n`;
   
+  // Apply a 15% discount to products in the inventory
   ProductProperties.applyDiscount(store.inventory, 0.15);
   
+  // Show inventory value after discount
   inventoryOutput.innerText += `After 15% Discount: $${store.getInventoryValue().toFixed(2)}\n\n`;
   
-
+  // Function to search for a prodcut by name
   function searchProduct() {
+    // Get user input
     const searchInput = document.getElementById("searchInput").value.trim();
+    // Search for product in the store
     const result = store.findProductByName(searchInput);
   
+    // Display result of search
     searchOutput.innerText = `Search: ${searchInput}\n`;
     searchOutput.innerText += result ? result.toString() : "Product not found.";
   }
