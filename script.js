d49485c626230ca6d41aa836ed556eb6b8ce2f80
Ryan Umoso <ryanumoso04@gmail.com>
2025-04-20 08:35:56 -0400

    Add PerishableProductProperties subclass

--- script.js
+++ script.js
@@ -16,3 +16,15 @@ class ProductProperties {
         products.forEach(p => p.price -= p.price * discount);
       }
 }
+
+class PerishableProductProperties extends ProductProperties {
+    constructor(name, price, quantity, expirationDate) {
+        super(name, price, quantity);
+        this.expirationDate = expirationDate;
+      }
+    
+      toString() {
+        return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
+      }
+}
+    
